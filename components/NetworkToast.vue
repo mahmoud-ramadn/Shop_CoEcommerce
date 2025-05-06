c874208ffcf49948c973f09a7bfc67aa517f852f
mahmoud-ramadn <ahmedshawareb567@gmail.com>
2025-05-06 15:33:58 +0300

    add-new network icon

--- components/NetworkToast.vue
+++ components/NetworkToast.vue
@@ -4,7 +4,7 @@
     <div 
       :class="{
         'bg-green-500 text-white': toastType === 'success',
-        'bg-red-500 text-white': toastType === 'error'
+        ' bg-gray-400 text-white': toastType === 'error'
       }"
       class="px-6 py-3 rounded-lg shadow-lg flex items-center animate-fade-in"
     >
@@ -14,9 +14,9 @@
         @click="dismissToast"
         class="ml-4 p-1 rounded-full hover:bg-white/20"
       >
-        <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" viewBox="0 0 20 20" fill="currentColor">
-          <path fill-rule="evenodd" d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z" clip-rule="evenodd" />
-        </svg>
+      <svg xmlns="http://www.w3.org/2000/svg" width="18px" height="18px" viewBox="0 0 24 24">
+	<path fill="none" stroke="#08ea63" stroke-linecap="round" stroke-linejoin="round" stroke-width="1.5" d="M14 8a2 2 0 1 1-4 0a2 2 0 0 1 4 0m2.959-3C17.619 5.87 18 6.898 18 8s-.381 2.13-1.041 3M7.04 5C6.381 5.87 6 6.898 6 8s.381 2.13 1.041 3m13.275-8C21.38 4.43 22 6.15 22 8s-.62 3.57-1.684 5M3.684 3C2.62 4.43 2 6.15 2 8s.62 3.57 1.684 5M11 10L7 21m10 0l-4-11m-4.5 7h7" color="#08ea63" />
+</svg>
       </button>
     </div>
   </div>
